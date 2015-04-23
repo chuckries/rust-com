0837e1d12963c3d96dfe2024792dd623fe5bebad
Chuck Ries <chuck.ries@gmail.com>
2015-04-23 00:17:13 -0700

    get building with rust 1.0.0 beta and Cargo
    add README
    
    add .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+based on https://gist.github.com/DanielKeep/5b4f613b22add300ee52
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ Shows the "Run" dialog under Windows using COM automation.
 
 */
 #![allow(dead_code)]
-#![feature(macro_rules)]
-#![license = "MIT"]
 
 extern crate libc;
 
-macro_rules! DEFINE_GUID(
+macro_rules! DEFINE_GUID{
 	($name:ident, $l:expr, $w1:expr, $w2:expr, $($bs:expr),+) => {
 		pub static $name: ::types::GUID = ::types::GUID {
 			data1: $l,
@@ -18,7 +16,7 @@ macro_rules! DEFINE_GUID(
 			data4: [$($bs),+]
 		};
 	};
-)
+}
 
 fn main() {
 	/*
@@ -30,9 +28,9 @@ fn main() {
 	win32::show_run_file_dialog();
 }
 
-fn fix_corrupt_tlb() {
-	unsafe { ::std::rt::stack::record_sp_limit(0); }
-}
+//fn fix_corrupt_tlb() {
+//	unsafe { ::std::rt::stack::record_sp_limit(0); }
+//}
 
 mod win32 {
 	use libc::c_void;
@@ -41,13 +39,13 @@ mod win32 {
 
 	pub fn show_run_file_dialog() {
 		use std::mem::transmute;
-		use std::ptr::mut_null;
+		use std::ptr::null_mut;
 		use types::{IID_IShellDispatch, IShellDispatch};
 
-		match unsafe { CoInitializeEx(mut_null(), COINIT_APARTMENTTHREADED) } {
+		match unsafe { CoInitializeEx(null_mut(), COINIT_APARTMENTTHREADED) } {
 			S_OK => (),
 			S_FALSE => (),
-			result => fail!("call to CoInitializeEx failed: {}", result)
+			result => panic!("call to CoInitializeEx failed: {}", result)
 		}
 
 		/*
@@ -59,20 +57,20 @@ mod win32 {
 
 		Not *widely* useful, especially not in idiomatic Rust, but still nice for dealing with these sorts of APIs.
 		*/
-		let mut obj: *mut IShellDispatch = mut_null();
-		match unsafe { CoCreateInstance(&CLSID_ShApp, mut_null(), CLSCTX_INPROC_SERVER, &IID_IShellDispatch, transmute(&mut obj)) } {
+		let mut obj: *mut IShellDispatch = null_mut();
+		match unsafe { CoCreateInstance(&CLSID_ShApp, null_mut(), CLSCTX_INPROC_SERVER, &IID_IShellDispatch, transmute(&mut obj)) } {
 			S_OK => (),
-			REGDB_E_CLASSNOTREG => fail!("CoCreateInstance failed: class not registered"),
-			E_NOINTERFACE => fail!("CoCreateInstance failed: class does not implement interface"),
-			0x800703e6 => fail!("CoCreateInstance failed: ERROR_NOACCESS; see https://github.com/rust-lang/rust/issues/13259"),
-			result => fail!("CoCreateInstance failed: error {:#08x}", result)
+			REGDB_E_CLASSNOTREG => panic!("CoCreateInstance failed: class not registered"),
+			E_NOINTERFACE => panic!("CoCreateInstance failed: class does not implement interface"),
+			0x800703e6 => panic!("CoCreateInstance failed: ERROR_NOACCESS; see https://github.com/rust-lang/rust/issues/13259"),
+			result => panic!("CoCreateInstance failed: error {:#08x}", result)
 		}
 
-		assert!(obj != mut_null());
+		assert!(obj != null_mut());
 
 		match unsafe { ((*(*obj).__vtable).FileRun)(transmute(obj)) } {
 			S_OK => (),
-			result => fail!("IShellDispatch.FileRun failed: error {:#08x}", result)
+			result => panic!("IShellDispatch.FileRun failed: error {:#08x}", result)
 		}
 
 		unsafe {
@@ -91,47 +89,47 @@ mod win32 {
 		fn CoUninitialize();
 	}
 
-	static CLSCTX_INPROC_SERVER: DWORD = 0x1;
-	static CLSCTX_INPROC_HANDLER: DWORD = 0x2;
-	static CLSCTX_LOCAL_SERVER: DWORD = 0x4;
-	static CLSCTX_INPROC_SERVER16: DWORD = 0x8;
-	static CLSCTX_REMOTE_SERVER: DWORD = 0x10;
-	static CLSCTX_INPROC_HANDLER16: DWORD = 0x20;
-	static CLSCTX_RESERVED1: DWORD = 0x40;
-	static CLSCTX_RESERVED2: DWORD = 0x80;
-	static CLSCTX_RESERVED3: DWORD = 0x100;
-	static CLSCTX_RESERVED4: DWORD = 0x200;
-	static CLSCTX_NO_CODE_DOWNLOAD: DWORD = 0x400;
-	static CLSCTX_RESERVED5: DWORD = 0x800;
-	static CLSCTX_NO_CUSTOM_MARSHAL: DWORD = 0x1000;
-	static CLSCTX_ENABLE_CODE_DOWNLOAD: DWORD = 0x2000;
-	static CLSCTX_NO_FAILURE_LOG: DWORD = 0x4000;
-	static CLSCTX_DISABLE_AAA: DWORD = 0x8000;
-	static CLSCTX_ENABLE_AAA: DWORD = 0x10000;
-	static CLSCTX_FROM_DEFAULT_CONTEXT: DWORD = 0x20000;
-	static CLSCTX_ACTIVATE_32_BIT_SERVER: DWORD = 0x40000;
-	static CLSCTX_ACTIVATE_64_BIT_SERVER: DWORD = 0x80000;
-	static CLSCTX_ENABLE_CLOAKING: DWORD = 0x100000;
-	static CLSCTX_APPCONTAINER: DWORD = 0x400000;
-	static CLSCTX_ACTIVATE_AAA_AS_IU: DWORD = 0x800000;
-	static CLSCTX_PS_DLL: DWORD = 0x80000000;
+	const CLSCTX_INPROC_SERVER: DWORD = 0x1;
+	const CLSCTX_INPROC_HANDLER: DWORD = 0x2;
+	const CLSCTX_LOCAL_SERVER: DWORD = 0x4;
+	const CLSCTX_INPROC_SERVER16: DWORD = 0x8;
+	const CLSCTX_REMOTE_SERVER: DWORD = 0x10;
+	const CLSCTX_INPROC_HANDLER16: DWORD = 0x20;
+	const CLSCTX_RESERVED1: DWORD = 0x40;
+	const CLSCTX_RESERVED2: DWORD = 0x80;
+	const CLSCTX_RESERVED3: DWORD = 0x100;
+	const CLSCTX_RESERVED4: DWORD = 0x200;
+	const CLSCTX_NO_CODE_DOWNLOAD: DWORD = 0x400;
+	const CLSCTX_RESERVED5: DWORD = 0x800;
+	const CLSCTX_NO_CUSTOM_MARSHAL: DWORD = 0x1000;
+	const CLSCTX_ENABLE_CODE_DOWNLOAD: DWORD = 0x2000;
+	const CLSCTX_NO_FAILURE_LOG: DWORD = 0x4000;
+	const CLSCTX_DISABLE_AAA: DWORD = 0x8000;
+	const CLSCTX_ENABLE_AAA: DWORD = 0x10000;
+	const CLSCTX_FROM_DEFAULT_CONTEXT: DWORD = 0x20000;
+	const CLSCTX_ACTIVATE_32_BIT_SERVER: DWORD = 0x40000;
+	const CLSCTX_ACTIVATE_64_BIT_SERVER: DWORD = 0x80000;
+	const CLSCTX_ENABLE_CLOAKING: DWORD = 0x100000;
+	const CLSCTX_APPCONTAINER: DWORD = 0x400000;
+	const CLSCTX_ACTIVATE_AAA_AS_IU: DWORD = 0x800000;
+	const CLSCTX_PS_DLL: DWORD = 0x80000000;
 
-	static COINIT_APARTMENTTHREADED: DWORD = 0x2;
-	static COINIT_MULTITHREADED: DWORD = 0x0;
-	static COINIT_DISABLE_OLE1DDE: DWORD = 0x4;
-	static COINIT_SPEED_OVER_MEMORY: DWORD = 0x8;
+	const COINIT_APARTMENTTHREADED: DWORD = 0x2;
+	const COINIT_MULTITHREADED: DWORD = 0x0;
+	const COINIT_DISABLE_OLE1DDE: DWORD = 0x4;
+	const COINIT_SPEED_OVER_MEMORY: DWORD = 0x8;
 
-	static S_OK: HRESULT = 0x00000000;
-	static S_FALSE: HRESULT = 0x00000001;
+	const S_OK: HRESULT = 0x00000000;
+	const S_FALSE: HRESULT = 0x00000001;
 
-	static CLASS_E_NOAGGREGATION: HRESULT = 0x80040110;
-	static E_NOINTERFACE: HRESULT = 0x80004002;
-	static REGDB_E_CLASSNOTREG: HRESULT = 0x80040154;
-	static REGDB_E_IIDNOTREG: HRESULT = 0x80040155;
-	static RPC_E_CHANGED_MODE: HRESULT = 0x80010106;
+	const CLASS_E_NOAGGREGATION: HRESULT = 0x80040110;
+	const E_NOINTERFACE: HRESULT = 0x80004002;
+	const REGDB_E_CLASSNOTREG: HRESULT = 0x80040154;
+	const REGDB_E_IIDNOTREG: HRESULT = 0x80040155;
+	const RPC_E_CHANGED_MODE: HRESULT = 0x80010106;
 
 	// {13709620-C279-11CE-A49E-444553540000}
-	DEFINE_GUID!(CLSID_ShApp, 0x13709620, 0xC279, 0x11CE, 0xA4,0x9E, 0x44,0x45,0x53,0x54,0x00,0x00)
+	DEFINE_GUID!{CLSID_ShApp, 0x13709620, 0xC279, 0x11CE, 0xA4,0x9E, 0x44,0x45,0x53,0x54,0x00,0x00}
 }
 
 #[allow(non_snake_case)]
@@ -143,7 +141,7 @@ mod types {
 		pub data1: u32,
 		pub data2: u16,
 		pub data3: u16,
-		pub data4: [u8, ..8],
+		pub data4: [u8; 8],
 	}
 
 	pub type BOOL = u32;
@@ -192,7 +190,7 @@ mod types {
 	}
 
 	//pub static IID_IUnknown: &'static str = "00000000-0000-0000-C000-000000000046";
-	DEFINE_GUID!(IID_IUnknown, 0x00000000, 0x0000, 0x0000, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46)
+	DEFINE_GUID!{IID_IUnknown, 0x00000000, 0x0000, 0x0000, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46}
 
 	#[repr(C)]
 	pub struct IUnknown_vtable {
@@ -207,7 +205,7 @@ mod types {
 	}
 
 	//pub static IID_IClassFactory: &'static str = "00000001-0000-0000-C000-000000000046";
-	DEFINE_GUID!(IID_IClassFactory, 0x00000001, 0x0000, 0x0000, 0xc0,0x00, 0x00,0x00,0x00,0x00,0x00,0x46)
+	DEFINE_GUID!{IID_IClassFactory, 0x00000001, 0x0000, 0x0000, 0xc0,0x00, 0x00,0x00,0x00,0x00,0x00,0x46}
 
 	#[repr(C)]
 	pub struct IClassFactory_vtable {
@@ -222,7 +220,7 @@ mod types {
 	}
 
 	//pub static IID_IDispatch: &'static str = "00020400-0000-0000-C000000000000046";
-	DEFINE_GUID!(IID_IDispatch, 0x00020400, 0x0000, 0x0000, 0xc0,0x00, 0x00,0x00,0x00,0x00,0x00,0x46)
+	DEFINE_GUID!{IID_IDispatch, 0x00020400, 0x0000, 0x0000, 0xc0,0x00, 0x00,0x00,0x00,0x00,0x00,0x46}
 
 	#[repr(C)]
 	pub struct IDispatch_vtable {
@@ -239,7 +237,7 @@ mod types {
 	}
 
 	//pub static IID_ITypeInfo: &'static str = "00020401-0000-0000-C000-000000000046";
-	DEFINE_GUID!(IID_ITypeInfo, 0x00020401, 0x0000, 0x0000, 0xc0,0x00, 0x00,0x00,0x00,0x00,0x00,0x46)
+	DEFINE_GUID!{IID_ITypeInfo, 0x00020401, 0x0000, 0x0000, 0xc0,0x00, 0x00,0x00,0x00,0x00,0x00,0x46}
 
 	#[repr(C)]
 	//#[idl="oaidl.idl"]
@@ -254,7 +252,7 @@ mod types {
 	}
 
 	//pub static IID_IShellDispatch: &'static str = "d8f015c0-c278-11ce-a49e-444553540000";
-	DEFINE_GUID!(IID_IShellDispatch, 0xd8f015c0, 0xc278, 0x11ce, 0xa4,0x9e, 0x44,0x45,0x53,0x54,0x00,0x00)
+	DEFINE_GUID!{IID_IShellDispatch, 0xd8f015c0, 0xc278, 0x11ce, 0xa4,0x9e, 0x44,0x45,0x53,0x54,0x00,0x00}
 
 	#[repr(C)]
 	//#[idl="shldisp.idl"]
